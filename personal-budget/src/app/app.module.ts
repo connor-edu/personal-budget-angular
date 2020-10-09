79eace738edd66b60ab2f9f8dd73da218acb42fd
Connor Love <connor@dotconnor.com>
2020-10-09 18:33:10 -0400

    fix quotes

--- personal-budget/src/app/app.module.ts
+++ personal-budget/src/app/app.module.ts
@@ -11,8 +11,8 @@ import { ArticleComponent } from "./article/article.component";
 import { AboutComponent } from "./about/about.component";
 import { LoginComponent } from "./login/login.component";
 import { P404Component } from "./p404/p404.component";
-import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
-import { ContactComponent } from './contact/contact.component';
+import { BreadcrumbsComponent } from "./breadcrumbs/breadcrumbs.component";
+import { ContactComponent } from "./contact/contact.component";
 
 @NgModule({
   declarations: [
