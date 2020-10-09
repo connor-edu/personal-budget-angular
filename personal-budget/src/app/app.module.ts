f57b8308245db49301b139319267dfe3ceb99043
Connor Love <connor@dotconnor.com>
2020-10-09 18:32:51 -0400

    add coding exercise 2 stuff

--- personal-budget/src/app/app.module.ts
+++ personal-budget/src/app/app.module.ts
@@ -11,6 +11,8 @@ import { ArticleComponent } from "./article/article.component";
 import { AboutComponent } from "./about/about.component";
 import { LoginComponent } from "./login/login.component";
 import { P404Component } from "./p404/p404.component";
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +25,8 @@ import { P404Component } from "./p404/p404.component";
     AboutComponent,
     LoginComponent,
     P404Component,
+    BreadcrumbsComponent,
+    ContactComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
---
+++ personal-budget/src/app/breadcrumbs/breadcrumbs.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "pb-breadcrumbs",
+  templateUrl: "./breadcrumbs.component.html",
+  styleUrls: ["./breadcrumbs.component.scss"],
+})
+export class BreadcrumbsComponent implements OnInit {
+  ngOnInit(): void {}
+}
